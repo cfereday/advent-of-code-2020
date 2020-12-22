56cc5b4779e07905450647a3dcccde07f0a01702
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-12-22 16:11:16 +0000

    Another experiment

--- src/1/day1Part2Attempt2.ts
+++ src/1/day1Part2Attempt2.ts
@@ -1,33 +1,57 @@
-// const productOfGivenNumbers = (numbers: number[]): number => {
-//     return numbers.reduce((accumulator, currentValue) => {
-//         return accumulator * currentValue
-//     });
-// };
+const addUpToBoundaryNumber = (currentNum, firstNum, secNum) => {
+    return currentNum + firstNum + secNum;
+}
 
+const getAllAdditions = (uniqueNumbers: number[], sumNumberToCheck, elements) => {
+    if (uniqueNumbers.length <= 2) {
+        // this can cause problems
+        console.log('I am less than 2');
+        return elements;
+    }
+    for (let i = 0; i < uniqueNumbers.length; i++) {
+        console.log('inside the loop, here is i', i);
+        const current = uniqueNumbers[i];
+        const next = uniqueNumbers[i + 1];
+        const nextNext = uniqueNumbers[i + 2];
+        const added = addUpToBoundaryNumber(current, next, nextNext);
 
-export const getNumbersAndProduct = (numbersToCheck: number[], sumNumberToCheck: number, boundaryNumber: number): object => {
+        if (uniqueNumbers.length === 3) {
+            if (added === sumNumberToCheck) {
+                elements.push(current, next, nextNext);
+                return elements;
+            } else {
+                return elements;
+            }
+        }
 
-    const addUpToBoundaryNumber = (currentNum, firstNum, secNum) => {
-        return currentNum + firstNum + secNum;
-    };
+        if (added === sumNumberToCheck) {
+            console.log('added does match');
+            elements.push(current, next, nextNext);
+            return elements;
+        }
 
-    const currentSet = new Set(numbersToCheck);
-    const uniqueNumbers = Array.from(currentSet);
-    
-    const elements = [];
-    const product = 0;
-
-    for (const myCurrentUniqueNumber of uniqueNumbers) {
-        // console.log('uniqueNumbers******', uniqueNumbers);
-        const sliced = uniqueNumbers.slice(0, boundaryNumber - 1);
-        const result = addUpToBoundaryNumber(myCurrentUniqueNumber, sliced[0], sliced[1]);
-        if (result === sumNumberToCheck) {
-            elements.push(myCurrentUniqueNumber, sliced[0], sliced[1]);
-            console.log('elements******', elements);
+        if (added !== sumNumberToCheck) {
+            console.log('no match');
+            return elements;
         }
     }
+};
 
-    const products = elements[0] * elements[1] * elements[2];
+export const getNumbersAndProduct = (numbersToCheck: number[], sumNumberToCheck: number): object => {
+    const currentSet = new Set(numbersToCheck);
+    const uniqueNumbers = Array.from(currentSet);
+    // remove duplicate entries by moving to a set & then back to an array
 
-    return {elements, product: products}
+    let elements = [];
+    console.log('length of uniqueNumbers****', uniqueNumbers.length);
+    elements = getAllAdditions(uniqueNumbers, sumNumberToCheck, elements);
+    console.log('elements', elements);
+
+    if (elements.length >= 3) {
+        console.log('i get here less than 3')
+        const products = elements[0] * elements[1] * elements[2];
+        return {elements, product: products}
+    } else {
+        return {elements, product: 0}
+    }
 };
