ffbb96944a22bd0e8eb717e3db2631d976c72b6a
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-12-23 11:16:17 +0000

    Adds test for Day 2

--- test/day2.test.ts
+++ test/day2.test.ts
@@ -1,7 +1,8 @@
 const {getValidPasswords} = require('../src/2/day2');
+const {testData} = require('./test-data-2');
 
 describe('Day 2', () => {
-    it.only('takes only valid passwords ', () => {
+    it('takes only valid passwords ', () => {
         const testInput = {
             '1-3 a': 'abcde',
             '1-3 b': 'cdefg',
@@ -9,5 +10,10 @@ describe('Day 2', () => {
         };
 
         expect(getValidPasswords(testInput)).toEqual(['abcde', 'ccccccccc']);
+    });
+
+    it('gets the answer for Day 2', () => {
+        const result = getValidPasswords(testData);
+        expect(result.length).toEqual(297)
     });
 });
