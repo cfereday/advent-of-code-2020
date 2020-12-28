504f75e444d982e4524245650e5121acb0b8f7b0
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-12-28 11:34:55 +0000

    Removes unecessary manipulation of string into object
    - Simplifies to split out a string from a number of strings

--- src/4/day4.ts
+++ src/4/day4.ts
@@ -1,40 +1,4 @@
-type optionalPassword = {
-    ecl?: string,
-    pid?: number,
-    eyr?: number,
-    hcl?: string,
-    byr?: number,
-    iyr?: number,
-    cid?: number,
-    hgt?: string
-}
-
-
-type optionalPasswordStrings = {
-    ecl?: string,
-    pid?: string,
-    eyr?: string,
-    hcl?: string,
-    byr?: string,
-    iyr?: string,
-    cid?: string,
-    hgt?: string
-}
-
-// type guards https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards could I just check on the type?
-interface ValidPassword {
-    ecl: string,
-    pid: number,
-    eyr: number,
-    hcl: string,
-    byr: number,
-    iyr: number,
-    cid: number,
-    hgt: string
-}
-
-// :todo how could I achieve this as an enum?
-const ValidPasswordEnum = [
+const mustHavePasswordKeys = [
     'ecl',
     'pid',
     'eyr',
@@ -44,35 +8,12 @@ const ValidPasswordEnum = [
     'hgt'
 ];
 
-const parseToNumber = (partOfPassword: string): number => parseInt(partOfPassword);
-
-const keysToParseToNumber = (obj: optionalPassword | ValidPassword): optionalPassword | ValidPassword => {
-    for (const [key, value] of Object.entries(obj)) {
-        if (key.match(/(pid|byr|cid|eyr|iyr)\b/))
-            obj[key] = parseToNumber(value);
-    }
-    //todo: figure out how to resolve this type issue
-    // @ts-ignore
-    return obj
-};
-
-const parseToObject = (password: string): optionalPassword | ValidPassword => {
-    const trimmedPassword = password.trim();
-    const splitPassword = trimmedPassword.split(' ');
-    const secondSplitPassword = splitPassword.map(p => {
-        const trimmed = p.trim();
-        return trimmed.split(':')
+const extractPasswords = (passwords: string): string[] => {
+    const split = passwords.split(/\n\n|\r\r/g);
+    return split.map(p => {
+        const stripped = p.replace(/\s+/g, " ");
+        return stripped.trim()
     });
-    //:todo i created a tsconfig with ESNext to resolve this error but perhaps some improvement to make as still have to ignore the tslint error for now: https://github.com/microsoft/TypeScript/issues/30933
-    // @ts-ignore
-    const obj: optionalPasswordStrings | ValidPassword = Object.fromEntries(secondSplitPassword);
-    // @ts-ignore
-    return keysToParseToNumber(obj);
-};
-
-const extractPasswords = (passwords: string): optionalPassword[] | ValidPassword[] => {
-    const split = passwords.split(/\n\n/g);
-    return split.map(p => parseToObject(p));
 };
 
 const isValid = (password: optionalPassword | ValidPassword): boolean => {
@@ -87,4 +28,4 @@ const getAllValidPasswordsPart1 = (passwords: string): ValidPassword[] => {
     return extracted.filter(p => isValid(p))
 };
 
-module.exports = {parseToObject, keysToParseToNumber, extractPasswords, isValid, getAllValidPasswordsPart1};
+module.exports = {extractPasswords, isValid, getAllValidPasswordsPart1};
--- test/day4.test.ts
+++ test/day4.test.ts
@@ -1,71 +1,8 @@
 const {parseToObject, keysToParseToNumber, extractPasswords, isValid, getAllValidPasswordsPart1} = require('../src/4/day4');
 const {testData} = require('./test-data-4');
 describe('Day 4', () => {
-    describe('keysToParseToNumber()', () => {
-        it('given a string value of a key returns a number if it exists', () => {
-            const myStringyObject = {
-                ecl: 'gry',
-                pid: '860033327',
-                eyr: '2020',
-                hcl: '#fffffd',
-                byr: '1937',
-                iyr: '2017',
-                cid: '147',
-                hgt: '183cm'
-            };
-
-            const myStringyAndNumberyObject = {
-                "byr": 1937,
-                "cid": 147,
-                "ecl": "gry",
-                "eyr": 2020,
-                "hcl": "#fffffd",
-                "hgt": "183cm",
-                "iyr": 2017,
-                "pid": 860033327
-            };
-
-            expect(keysToParseToNumber(myStringyObject)).toEqual(myStringyAndNumberyObject)
-        });
-
-        it('ignores any keys to parse which are not present', () => {
-            const myStringyObject = {
-                ecl: 'gry',
-                pid: '860033327',
-                hcl: '#fffffd',
-                byr: '1937',
-                cid: '147',
-                hgt: '183cm'
-            };
-
-            const myStringyAndNumberyObject = {
-                "ecl": "gry",
-                "pid": 860033327,
-                "hcl": "#fffffd",
-                "byr": 1937,
-                "cid": 147,
-                "hgt": "183cm",
-            };
-            expect(keysToParseToNumber(myStringyObject)).toEqual(myStringyAndNumberyObject)
-        });
-    });
-
-    describe('parseToObject', () => {
-        it('extracts a password string into a password object', () => {
-            const onePassword = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm';
-            expect(parseToObject(onePassword)).toEqual({
-                ecl: 'gry',
-                pid: 860033327,
-                eyr: 2020,
-                hcl: '#fffffd',
-                byr: 1937,
-                iyr: 2017,
-                cid: 147,
-                hgt: '183cm'
-            })
-        });
-
-        it('extracts a number of password strings into a password object', () => {
+    describe('parse', () => {
+        it('extracts a number of password strings into an array of passwords', () => {
             const input = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
             byr:1937 iyr:2017 cid:147 hgt:183cm
 
@@ -80,108 +17,52 @@ describe('Day 4', () => {
             hcl:#cfa07d eyr:2025 pid:166559648
             iyr:2011 ecl:brn hgt:59in`;
 
-            expect(extractPasswords(input)).toEqual([
-                {
-                    ecl: 'gry',
-                    pid: 860033327,
-                    eyr: 2020,
-                    hcl: '#fffffd',
-                    byr: 1937,
-                    iyr: 2017,
-                    cid: 147,
-                    hgt: '183cm'
-                },
-                {
-                    iyr: 2013,
-                    ecl: 'amb',
-                    cid: 350,
-                    eyr: 2023,
-                    pid: 28048884,
-                    hcl: '#cfa07d',
-                    byr: 1929,
-                },
-                {
-                    hcl: '#ae17e1',
-                    iyr: 2013,
-                    eyr: 2024,
-                    ecl: 'brn',
-                    pid: 760753108,
-                    byr: 1931,
-                    hgt: '179cm',
-                },
-                {
-                    hcl: '#cfa07d',
-                    eyr: 2025,
-                    pid: 166559648,
-                    iyr: 2011,
-                    ecl: 'brn',
-                    hgt: '59in',
-                }
-            ])
+            const result = extractPasswords(input);
+
+            expect(result).toEqual([
+                'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm',
+                'iyr:2013 ecl:amb cid:350 eyr:2023 pid:28048884 hcl:#cfa07d byr:1929',
+                'hcl:#ae17e1 iyr:2013e eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm',
+                'hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in'
+            ]);
+            expect(result.length).toEqual(4);
         });
-    });
 
     describe('isValid', () => {
-        it('checks if a password is valid according to p1 validity',() => {
-            const myStringyAndNumberyObject = {
-                "byr": 1937,
-                "cid": 147,
-                "ecl": "gry",
-                "eyr": 2020,
-                "hcl": "#fffffd",
-                "hgt": "183cm",
-                "iyr": 2017,
-                "pid": 860033327
-            };
+        it('checks if a password is valid according to p1 validity', () => {
+            const valid = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm';
 
-            expect(isValid(myStringyAndNumberyObject)).toEqual(true)
-        });
-
-        it('returns true if password is valid according to p1 validity',() => {
-            const myStringyAndNumberyObject = {
-                "byr": 1937,
-                "cid": 147,
-                "ecl": "gry",
-                "eyr": 2020,
-                "hcl": "#fffffd",
-                "hgt": "183cm",
-                "iyr": 2017,
-                "pid": 860033327
-            };
-
-            expect(isValid(myStringyAndNumberyObject)).toEqual(true)
-        });
-
-        it('returns false if password is not valid according to p1 validity', () => {
-            const myInvalidStringAndNumberObject = {
-                iyr: 2013,
-                ecl: 'amb',
-                cid: 350,
-                eyr: 2023,
-                pid: 28048884,
-                hcl: '#cfa07d',
-                byr: 1929,
-            };
-
-            expect(isValid(myInvalidStringAndNumberObject)).toEqual(false)
+            expect(isValid(valid)).toEqual(true)
         });
 
         it('returns true if password is valid according to p1 validity', () => {
-            const myValidStringAndNumberObject =  {
-                    hcl: '#ae17e1',
-                    iyr: 2013,
-                    eyr: 2024,
-                    ecl: 'brn',
-                    pid: 760753108,
-                    byr: 1931,
-                    hgt: '179cm',
-                };
+            const valid = 'iyr:2013 ecl:amb cid:350 eyr:2023 pid:28048884 hcl:#cfa07d byr:1929';
+
+            expect(isValid(valid)).toEqual(true)
+        });
+
+        it('returns false if password is not valid according to p1 validity', () => {
+            const invalid = 'hcl:#ae17e1 iyr:2013e eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm';
+
+            expect(isValid(invalid)).toEqual(false)
+        });
+
+        it('returns true if password is valid according to p1 validity', () => {
+            const myValidStringAndNumberObject = {
+                hcl: '#ae17e1',
+                iyr: 2013,
+                eyr: 2024,
+                ecl: 'brn',
+                pid: 760753108,
+                byr: 1931,
+                hgt: '179cm',
+            };
 
             expect(isValid(myValidStringAndNumberObject)).toEqual(true)
         });
 
         it('returns false if password is invalid according to p1 validity', () => {
-            const myOtherInvalidStringAndNumberObject =  {
+            const myOtherInvalidStringAndNumberObject = {
                 hcl: '#cfa07d',
                 eyr: 2025,
                 pid: 166559648,
@@ -214,7 +95,7 @@ describe('Day 4', () => {
         });
 
         it('gets the answers for day 4: p1', () => {
-           expect(getAllValidPasswordsPart1(testData).length).toEqual(31)
+            expect(getAllValidPasswordsPart1(testData).length).toEqual(31)
         });
     });
 
